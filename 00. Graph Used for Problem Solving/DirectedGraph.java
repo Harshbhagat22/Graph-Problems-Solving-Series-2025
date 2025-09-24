ef2d35e7310500eb52f7ed4aa8c31d03c51794b9
H A R S H  ! <145217050+Harshbhagat22@users.noreply.github.com>
2025-09-24 20:51:02 +0530

    Add files via upload

---
+++ 00. Graph Used for Problem Solving/DirectedGraph.java
@@ -0,0 +1,54 @@
+import java.util.*;
+public class DirectedGraph {
+    public static void main(String[]args){
+        Graph g = new Graph();
+        // Add Vertex
+        
+        g.addVertex("A");
+        g.addVertex("B");
+        g.addVertex("C");
+        g.addVertex("D");
+        g.addVertex("E");
+        
+        // Add Edge..
+        
+        g.addEdge("B", "A");
+        g.addEdge("B", "E");
+        g.addEdge("C", "A");
+        g.addEdge("C", "D");
+        g.addEdge("D", "C");
+        g.addEdge("E", "D");
+        
+        // Display Graph..
+        
+        g.printGraph();
+    }
+}
+
+class Graph {
+    HashMap<String,List<String>>graph;
+        public Graph(){
+            graph = new HashMap<>();
+        }
+        
+        public void addVertex(String v){
+            graph.putIfAbsent(v,new ArrayList<>());
+        }
+        
+        public void addEdge(String v1, String v2){
+            graph.get(v1).add(v2);
+        }
+        
+        public void printGraph(){
+            for(String key : graph.keySet()){
+                System.out.print(key+" : ");
+                List<String> temp = graph.get(key);
+                if(temp.isEmpty())System.out.print("NULL");
+                    for(String buff : temp){
+                        System.out.print(buff+" ");
+                    }
+                    
+                System.out.println();
+            }
+        }
+}
